4c4a1d1a4ae0277799f939490395fc562fe0ec0e
Shrey Vernekar <glitchguru03@gmail.com>
2025-06-14 14:09:56 +0530

    created user registration and basic authentication

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 const dotenv = require('dotenv');
 dotenv.config(); 
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const app = express();
+const connectDB = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+
+
+connectDB(); // Connect to the database
+
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/users', userRoutes);
 
 
 module.exports = app;
