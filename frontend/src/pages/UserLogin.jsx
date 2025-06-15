042055ae57a87dd58adc740bb3f994904af1aa7e
Shrey Vernekar <glitchguru03@gmail.com>
2025-06-15 22:00:15 +0530

    WIP: Changed the look for userLogin and captainLogin

--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
 
 const UserLogin = () => {
-  return (
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [userData, setUserData] = useState({})
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setUserData({
+      email: email,
+      password: password
+    });
+    console.log(userData)
+    setEmail('');
+    setPassword('');
+  }
+
+   return (
     <div>
       <Header />
-      <div className='p-7 '>
+      <div className='p-7 h-screen flex flex-col justify-between'>
         <div>
-          <form action="">
+          <form action="" onSubmit={(e)=>{submitHandler(e)}} >
           <h3 className='text-lg font-medium mb-2'>What's Your Email?</h3>
           <input required 
+                 value={email}
+                 onChange={(e) => setEmail(e.target.value)}
                  className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placholder:text-base'
                  type="text" 
                  placeholder='email@example.com' 
           />
           <h3 className='text-lg font-medium mb-2'>Enter Password</h3>
           <input required 
+                 value={password}
+                 onChange={(e) => setPassword(e.target.value)}
                  className='bg-[#eeeeee] mb-7 rounded px-4 py-2 border w-full text-lg placholder:text-base'
                  type="password" 
                  placeholder='Password' 
@@ -25,11 +45,13 @@ const UserLogin = () => {
            className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2  w-full text-lg placholder:text-base'
           >Login</button>
         </form>
-        </div>
+        <p className='text-center'>Ready to join the ride? <Link to='/signup' className='text-blue-600'>Create Your Account</Link></p>
+        <p className='text-center mt-4'>Or <Link to='/google-signin' className='text-blue-600'>Sign in with Google</Link></p>
+        </div> 
         <div>
-          <button
-          className='bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2  w-full text-lg placholder:text-base'
-          >Sign in as Captain</button>
+          <Link
+          to='/captain-login' className='bg-[#10b461] flex items-center justify-center text-white font-semibold mb-7 rounded px-4 py-2  w-full text-lg placholder:text-base'
+          >Sign in as Captain</Link>
         </div>
       </div>
       <Footer />
