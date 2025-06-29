c91a77172c3dcf0abf780c8ba584805477468e82
Shrey Vernekar <glitchguru03@gmail.com>
2025-06-29 20:40:25 +0530

    Wip: implement CaptainHome layout with CaptainDetails and RidePopUp components

---
+++ frontend/src/components/CaptainDetails.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const CaptainDetails = () => {
+  return (
+    <div>
+      <div className='flex items-center justify-between'>
+          <div className='flex items-center justify-start gap-3'>
+            <img className='h-10 w-10 rounded-full object-cover' src="https://tse2.mm.bing.net/th/id/OIP.x6_OuR6j8XixmWyCqfyEKgHaHa?pid=Api&P=0&h=180" alt="" />
+            <h4 className='text-lg font-medium'>Karen Lobo</h4>
+          </div>
+          <div>
+            <h4 className='text-lx font-semibold'>Rs. 269.50</h4>
+            <p className=' text-sm font-medium text-gray-600'>Earned</p>
+          </div>
+        </div>
+        <div className='flex p-3 mt-6 bg-gray-50 rounded-full justify-center gap-4 items-start'>
+          <div className='text-center'><i className="text-2xl  mb-2 font-thin ri-timer-2-line"></i>
+            <h5 className='text-lg font-medium'>10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p></div>
+          <div className='text-center'><i className=" text-2xl mb-2 font-thin ri-speed-up-line"></i>
+            <h5 className='text-lg font-medium'>10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p></div>
+          <div className='text-center'><i className=" text-2xl mb-2 font-thin ri-booklet-line"></i>
+            <h5 className='text-lg font-medium' >10.2</h5>
+            <p className='text-sm text-gray-600'>Hours Online</p></div>
+        </div>
+    </div>
+  )
+}
+
+export default CaptainDetails
--- frontend/src/pages/CaptainHome.jsx
+++ frontend/src/pages/CaptainHome.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import { Link } from 'react-router-dom'
+import VexaLogo from '../images/vexalogo.png'
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import Car from '../images/car.png';
+import 'remixicon/fonts/remixicon.css'
+import CaptainDetails from '../components/CaptainDetails';
+import RidePopUp from '../components/RidePopUp';
 
 const CaptainHome = () => {
+
+  const ridePopUpPanelRef = useRef(null)
+  const [ridePopUpPanel, SetRidePopUpPanel] = useState(true)
+
+  useGSAP(() => {
+    if (ridePopUpPanel) {
+      gsap.to(ridePopUpPanelRef.current, {
+        transform: 'translateY(0%)'
+      });
+    } else {
+      gsap.to(ridePopUpPanelRef.current, {
+        transform: 'translateY(100%)'
+      });
+    }
+  }, [ridePopUpPanel]);
+
   return (
-    <div>
-      Captain Home
+    <div className='h-screen'>
+      <div className='fixed p-6 top-0 flex items-center justify-between w-screen'>
+        <img className='w-16' src={VexaLogo} alt="" />
+        <Link to='/home' className='h-10 w-10 bg-white flex items-center justify-center rounded-full '>
+          <i className=" text-lg font-medium ri-logout-box-line"></i>
+        </Link>
+      </div>
+      <div className='h-3/5'>
+        <img className='h-full w-full object-cover' src="https://miro.medium.com/max/1280/0*gwMx05pqII5hbfmX.gif" alt="" />
+
+      </div>
+      <div className='h-2/5 p-6'>
+        <CaptainDetails />
+      </div>
+      <div ref={ridePopUpPanelRef} className='fixed w-full z-10 bottom-0 translate-y-full bg-white px-5 py-6 pt-12'>
+        <RidePopUp SetRidePopUpPanel= {SetRidePopUpPanel} />
+      </div>
     </div>
   )
 }
